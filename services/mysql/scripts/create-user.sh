01005e64035927ece7acbfc0fa68709cd3b02847
Stancu Florin <niflostancu@gmail.com>
2020-05-31 14:06:24 +0300

    mysql enhancements / fixes + create-user script
    - easier to debug / repair database issues;
    - higher health / readiness check timeouts;
    - create-user: use existing / new secret;

--- services/mysql/scripts/create-user.sh
+++ services/mysql/scripts/create-user.sh
@@ -5,7 +5,6 @@ set -e
 secretName="$1"
 username="$2"
 database="$3"
-password=$(read -p "User's password: " -s pwd; echo -n "$pwd")
 
 [[ -n "$secretName" && -n "$username" && -n "$database" ]] || {
 	echo "Syntax: create-user.sh secret-name username database" >&2
@@ -14,10 +13,20 @@ password=$(read -p "User's password: " -s pwd; echo -n "$pwd")
 
 SDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-kubectl create secret generic "$secretName" \
-	--from-literal="mysql-user=$username" \
-	--from-file="mysql-password="<(echo -n "$password")
+if kubectl get secret "$secretName" >/dev/null 2>&1; then
+	password=$(kubectl get secret "$secretName" --template="{{ index .data \"mysql-password\" }}" | base64 --decode)
+else
+	password=$(read -p "User's password: " -s pwd; echo -n "$pwd")
+	kubectl create secret generic "$secretName" \
+		--from-literal="mysql-user=$username" \
+		--from-file="mysql-password="<(echo -n "$password")
+fi
 
-echo "GRANT ALL PRIVILEGES ON $database.* TO '$username'@'%' IDENTIFIED BY '$password';" #| \
-	"$SDIR"/client.sh
+QUERY="CREATE USER '$username'@'%' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON $database.* TO '$username'@'%';"
+if [[ -n "$SHOW_QUERY" ]]; then
+	echo "$QUERY"
+else
+	echo "$QUERY" | "$SDIR"/client.sh
+fi
 
